89aad822096977fcc744e140a747dc16d0f89417
TKDefender88 <tkdenfeder@proton.me>
2026-06-09 10:06:20 -0600

    create a multiwriter logger that logs to files and stderr

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -24,24 +27,54 @@ func main() {
 	os.Exit(status)
 }
 
+func initializeLogger(logFile string) (*log.Logger, func() error, error) {
+	const logPrefix = ""
+
+	if logFile != "" {
+		fd, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+		if err != nil {
+			return nil, nil, fmt.Errorf("error opening the log file: %w", err)
+		}
+
+		bufferedFile := bufio.NewWriterSize(fd, 8192)
+
+		closer := func() error {
+			if err := bufferedFile.Flush(); err != nil {
+				return fmt.Errorf("error flushing the log buffer: %w", err)
+			}
+			if err := fd.Close(); err != nil {
+				return fmt.Errorf("error closing the log file: %w", err)
+			}
+			return nil
+		}
+
+		mWriter := io.MultiWriter(bufferedFile, os.Stderr)
+		return log.New(mWriter, logPrefix, log.LstdFlags), closer, nil
+	}
+
+	noOpCloser := func() error { return nil }
+	return log.New(os.Stderr, logPrefix, log.LstdFlags), noOpCloser, nil
+}
+
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
 
-	logFile, err := os.OpenFile("linko.access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	logger, closer, err := initializeLogger(os.Getenv("LINKO_LOG_FILE"))
 	if err != nil {
-		return 44
+		fmt.Fprintf(os.Stderr, "failed to initialize logger: %v", err)
+		return 1
 	}
-	defer logFile.Close()
-	accessLogger := log.New(logFile, "INFO: ", log.LstdFlags)
+	defer func() {
+		if err := closer(); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to close logger: %v", err)
+		}
+	}()
 
-	logger := log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
-	//logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
-	st, err := store.New(dataDir, accessLogger)
+	st, err := store.New(dataDir, logger)
 	if err != nil {
 		logger.Printf("failed to create store: %v\n", err)
 		return 1
 	}
-	s := newServer(*st, httpPort, cancel, accessLogger)
+	s := newServer(*st, httpPort, cancel, logger)
 	var serverErr error
 	go func() {
 		logger.Printf("Linko is running on http://localhost:%d", httpPort)
