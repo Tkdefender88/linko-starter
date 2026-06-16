a12d92343691d6b2bc367a2ae0b4c1aa9f327401
Justin Bak (╯°□°)╯︵ ┻━┻ <me@justinbak.com>
2026-06-16 09:16:54 -0600

    add profiling

--- server.go
+++ server.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"os"
 
+	"net/http/pprof"
+
 	"boot.dev/linko/internal/store"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -43,6 +45,8 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 	mux.HandleFunc("GET /{shortCode}", s.handlerRedirect)
 	mux.HandleFunc("POST /admin/shutdown", s.handlerShutdown)
 	mux.Handle("GET /metrics", promhttp.Handler())
+	mux.Handle("GET /debug/pprof/", s.authMiddleware(http.HandlerFunc(pprof.Index)))
+	mux.Handle("GET /debug/pprof/profile", s.authMiddleware(http.HandlerFunc(pprof.Profile)))
 
 	return s
 }
